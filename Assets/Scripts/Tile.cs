bc739c1d09f2945cd4a587041352f3e710997da5
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2021-09-17 15:18:39 +0200

    Lesson 1 pfp Tiles done

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isBuildable;
+
+
+    public bool GetIsBuildable()
+    {
+        return _isBuildable;
+    }
+}
